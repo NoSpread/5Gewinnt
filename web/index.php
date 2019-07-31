5434aa03bbd655c7a56b26ba1f0bbd1af2aafce0
Flo <inf18161@lehre.dhbw-stuttgart.de>
2019-07-31 08:57:20 +0200

    Add Lobby

--- web/index.php
+++ web/index.php
@@ -1,6 +1,63 @@
 <?php
-session_start();
-require_once 'res/php/config.php';
-require_once 'res/includes/auth_validate.php';
-$db->getInstance();
+//session_start();
+//require_once 'res/php/config.php';
+//require_once 'res/includes/auth_validate.php';
+//$db->getInstance();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+
+    <title>Lobby</title>
+
+    </head>
+<body id="body">
+    LOBBY!!1!
+	<br/>
+	<table>
+	<tr>
+		<th>
+			<div id="gamelist">
+				<button id="update" type="submit" onclick="opengame()">Update!</button>
+				<script language="Javascript">
+					function opengame(){
+						document.getElementById("ogames").innerHTML = "";
+						
+						}
+				</script>
+				
+				<select id="ogames" name="opengames">
+					<?php 
+						for($i = 0; $i < 10; $i++){
+							//DB Abfrage 
+							echo "<option seleced=\"selected\">Spiel". $i . "</option>";
+						}
+					?>
+				</select>
+				<button id="join" type="submit">Join Game</button>
+				
+			</div>
+		</th>
+		<th>
+			Join with Game Code!
+			<form include="pages/index.php">
+			
+	</tr>
+	<form action="pages/index.php" method="post">
+		<select name="gamemode" size="2">
+			<option selected^="selected">public</option>
+			<option>private</option>
+		</select>
+		
+		<button id="create" type="submit">Create Game</button>
+	</form>
+	<form action="pages/index.php" method="post">
+		<input type="text" id="gameid" placeholder="gameid" required>
+		<button id="search" type="submit">suchen</button>
+	</form>
+	<form action="pages/index.php" method="post">
+		<button id="remove" type="submit">remove invite</button>
+	</form>
+</body>
+</html>
--- web/pages/index.php
+++ web/pages/index.php
@@ -1,3 +1,30 @@
 <?php
-require_once '../res/php/helpers.php';
-redirect_index();
+//require_once '../res/php/helpers.php';
+//redirect_index();
+?>
+
+<body id="body">
+    LOBBY!!1!
+	
+	<alert>
+	<?php
+	if(isset($_POST['remove'])){
+		//remove in Table id where userid = hostid
+	}
+	if(isset($_POST['create'])){
+
+		echo '<script language="Javascript">';
+		echo 'alert("Spiel erstellt"); ';
+		echo ' </script>';
+	}
+	else{
+		
+		echo '<script language="Javascript">';
+		echo 'alert("not hot"); ';
+		echo ' </script>';
+		
+	}
+	?>
+
+</body>
+</html>
