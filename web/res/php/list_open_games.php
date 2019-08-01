5f9c3eb59480274ba4ed020be1e02bee62dfc383
Flo <inf18161@lehre.dhbw-stuttgart.de>
2019-08-01 14:45:09 +0200

    Bugfix

--- web/res/php/list_open_games.php
+++ web/res/php/list_open_games.php
@@ -4,7 +4,7 @@ require_once 'config.php';
 
 $db = getDbInstance();
 
-$open_games = $db->query('SELECT id, player1 FROM game WHERE state="open"');
+$open_games = $db->query('SELECT id, player1 FROM game WHERE player2 IS NULL');
 
 echo json_encode($open_games);
 
