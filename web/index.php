49008809138022cbbfa91577f0dd9ad2c7603d77
Lukas Naatz <inf18117@lehre.dhbw-stuttgart.de>
2019-08-01 11:14:33 +0200

    Restructured project

--- web/index.php
+++ web/index.php
@@ -1,58 +1,117 @@
-<?php
-//session_start();
-//require_once 'res/php/config.php';
-//require_once 'res/includes/auth_validate.php';
-//$db->getInstance();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<html>
+    <head>
+        <title>Game Lobby</title>
+        <script>
+            /**
+              * @param game Game object to be added to the game table
+              */
+            function addGame(game) {
+                var table = document.getElementById('gameTable');
 
+                var tableRow = document.createElement('tr');
+                table.appendChild(tableRow);
 
-    <title>Lobby</title>
+                var idCell = document.createElement('td');
+                tableRow.appendChild(idCell);
 
+                var linkCell = document.createElement('a');
+                linkCell.href = 'play.php?id=' + game.id;
+                idCell.appendChild(linkCell);
+                linkCell.appendChild(document.createTextNode(game.id))
+
+                var playerCell = document.createElement('td');
+                tableRow.appendChild(playerCell);
+                playerCell.appendChild(document.createTextNode(game.player1));
+
+                loadedGameIds.push(game.id);
+            }
+
+            /**
+              * @param id Game id to be removed from the game table
+              */
+            function removeGame(id) {
+                var tableRows = document.getElementById('gameTable').children;
+
+                // Iterate over game table, search for the given game id and remove the entry
+                for (let i = 0; i < games.length; i++) {
+                    var curId = tableRows[i] // <tr> tag
+                        .children[0] // first <td> tag ([0] -> game id, [1] -> player1)
+                        .firstChild // <a> tag
+                        .firstChild // text node
+                        .nodeValue; // game id
+
+                    if (curId == id) {
+                        table.removeChild(games[i]);
+
+                        loadedGameIds.splice(loadedGameIds.indexOf(id), 1);
+                        return;
+                    }
+                }
+            }
+
+            /**
+              * Request a json object with all open games from the server and list them in the game table
+              */
+            function updateGames() {
+                var xhttp = new XMLHttpRequest();
+
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        var games = JSON.parse(this.responseText);
+
+                        // Create a copy of all loaded game ids and remove those that are still open
+                        // The games that are left after that need to be removed from the game table
+                        var oldGameIds = loadedGameIds.slice();
+
+                        for (let i = 0; i < games.length; i++) {
+                            var game = games[i];
+                            if (!loadedGameIds.includes(game.id)) {
+                                addGame(game);
+                            } else {
+                                oldGameIds.splice(oldGameIds.indexOf(game.id), 1);
+                            }
+                        }
+
+                        for (let i = 0; i < oldGameIds.length; i++) {
+                            remove(oldGameIds[i]);
+                        }
+                    }
+                };
+
+                xhttp.open('GET', '../res/php/list_open_games.php', true);
+                xhttp.send();
+            }
+
+            /**
+              * Set an interval that updates the game list every second
+              */
+            function startUpdateLoop() {
+                setInterval(updateGames, 1000);
+            }
+
+            /**
+              * Add a fresh new game to the database
+              */
+            function createGame() {
+                var xhttp = new XMLHttpRequest();
+                xhttp.open('GET', '../res/php/create_game.php', true);
+                xhttp.send();
+            }
+
+            var loadedGameIds = [];
+        </script>
     </head>
-<body id="body" onload="getglist()">
-    LOBBY!!1!
-	<br/>
-	<div id="joingame">
-		<form>
-			<button id="update" type="submit" onclick="getglist()">Update!</button>
-				<script>
-					function getglist() {
-						  var xhttp = new XMLHttpRequest();
-						  xhttp.onreadystatechange = function() {
-							if (this.readyState == 4 && this.status == 200) {
-							 document.getElementById("ogames").innerHTML = this.responseText;
-							}
-						  };
-						  xhttp.open("GET", "res/php/list_open_games.php", true);
-						  xhttp.send();
-					}
-				</script>
-			<table id="ogames" name="opengames">
-			</table>
-		</form>
-		<form>
-			<input type="text" placeholder="123465789">
-			<button id="join" type="submit" onclick="findg()">Join Game</button>
-
-			<script>
-				function findg(){
-					var xhttp = new XMLHttpRequest();
-					  xhttp.onreadystatechange = function() {
-						if (this.readyState == 4 && this.status == 200) {
-						 alert(this.responseText);
-						}
-					  };
-					  xhttp.open("GET", "res/php/findgame.php", true);
-					  xhttp.send();
-				}
-			</script>
-		</form>
-	</div>
-	<form action="pages/creategame oder so.php" method="post">
-		<button id="create" type="submit">Create Game</button>
-	</form>
-</body>
+    <body onload='startUpdateLoop();'>
+        <h1>Game Lobby &#127976;</h1>
+        <table border='1'>
+            <thead>
+                <tr>
+                    <th>Game ID</th>
+                    <th>Challenging Player</th>
+                <tr>
+            </thead>
+            <tbody id='gameTable'></tbody>
+        </table>
+        <button onclick='createGame();'>Create new game</button>
+    </body>
 </html>
