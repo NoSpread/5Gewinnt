301ed970c62a2cee428cca4adf2da2f11747369d
Lukas Naatz <inf18117@lehre.dhbw-stuttgart.de>
2019-08-02 08:41:36 +0200

    Cleaned

--- web/res/php/list_open_games.php
+++ web/res/php/list_open_games.php
@@ -5,7 +5,6 @@ require_once 'config.php';
 $db = getDbInstance();
 
 $open_games = $db->query('SELECT game.id, game.player1, game.player2, user.username FROM game INNER JOIN user ON COALESCE(game.player1, game.player2) = user.id WHERE (player1 IS NULL) OR (player2 IS NULL)');
-
 echo json_encode($open_games);
 
 ?>
