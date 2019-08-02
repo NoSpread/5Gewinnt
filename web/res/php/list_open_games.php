cfa8f1b7fab078c800616a41419c8c78e4deebb4
Lukas Naatz <inf18117@lehre.dhbw-stuttgart.de>
2019-08-02 08:41:36 +0200

    Cleaned

--- web/res/php/list_open_games.php
+++ web/res/php/list_open_games.php
@@ -4,8 +4,6 @@ require_once 'config.php';
 
 $db = getDbInstance();
 
-$db->query("set collation_connection='utf16_unicode_ci'"); 
-
 $open_games = $db->query('SELECT game.id, game.player1, game.player2, user.username FROM game INNER JOIN user ON (game.player1 = user.id) OR (game.player2 = user.id) WHERE (player1 IS NULL) OR (player2 IS NULL)');
 
 echo json_encode($open_games);
