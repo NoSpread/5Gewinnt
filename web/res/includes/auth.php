90e7d45efac7bdd0f536f5009c160ad10da859b4
Simon <inf18165@lehre.dhbw-stuttgart.de>
2019-08-04 12:09:12 +0200

    Code-Cleanup in auth.php

--- web/res/includes/auth.php
+++ web/res/includes/auth.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../php/config.php';
+require_once 'rememberMe.php';
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -9,9 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 	//Get DB instance.
 	$db = getDbInstance();
-
 	$db->where("email", $emailorusername);
-
 	$row = $db->get('user');
 
 	if ($db->count >= 1) {
@@ -26,28 +25,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			$_SESSION['id'] = $row[0]['id'];
 
 			if ($remember) {
-
-				$series_id = randomString(16);
-				$remember_token = getSecureRandomToken(20);
-				$encryted_remember_token = password_hash($remember_token, PASSWORD_DEFAULT);
-				
-
-				$expiry_time = date('Y-m-d H:i:s', strtotime(' + 30 days'));
-
-				$expires = strtotime($expiry_time);
-				
-				setcookie('series_id', $series_id, $expires, "/");
-				setcookie('remember_token', $remember_token, $expires, "/");
-
-				$db = getDbInstance();
-				$db->where ('id', $user_id);
-
-				$update_remember = array(
-					'series_id'=> $series_id,
-					'remember_token' => $encryted_remember_token,
-					'expires' =>$expiry_time
-				);
-				$db->update('user', $update_remember);
+				// Erinnert sich an den User
+				rememberMe($user_id);
 			}
 			//Authentication successfull redirect user
 			header('Location:../../');
@@ -75,28 +54,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			$_SESSION['id'] = $row[0]['id'];
 
 			if ($remember) {
-
-				$series_id = randomString(16);
-				$remember_token = getSecureRandomToken(20);
-				$encryted_remember_token = password_hash($remember_token, PASSWORD_DEFAULT);
-				
-
-				$expiry_time = date('Y-m-d H:i:s', strtotime(' + 30 days'));
-
-				$expires = strtotime($expiry_time);
-				
-				setcookie('series_id', $series_id, $expires, "/");
-				setcookie('remember_token', $remember_token, $expires, "/");
-
-				$db = getDbInstance();
-				$db->where ('id', $user_id);
-
-				$update_remember = array(
-					'series_id'=> $series_id,
-					'remember_token' => $encryted_remember_token,
-					'expires' =>$expiry_time
-				);
-				$db->update('user', $update_remember);
+				//Erinnert sich an den User
+				rememberMe($user_id);
 			}
 			//Authentication successfull redirect user
 			header('Location:../../');
---
+++ web/res/includes/rememberMe.php
@@ -0,0 +1,24 @@
+<?php
+function rememberMe($user_id) {
+    $series_id = randomString(16);
+    $remember_token = getSecureRandomToken(20);
+    $encryted_remember_token = password_hash($remember_token, PASSWORD_DEFAULT);
+
+    $expiry_time = date('Y-m-d H:i:s', strtotime(' + 30 days'));
+
+    $expires = strtotime($expiry_time);
+    
+    setcookie('series_id', $series_id, $expires, "/");
+    setcookie('remember_token', $remember_token, $expires, "/");
+
+    $db = getDbInstance();
+    $db->where ('id', $user_id);
+
+    $update_remember = array(
+        'series_id'=> $series_id,
+        'remember_token' => $encryted_remember_token,
+        'expires' =>$expiry_time
+    );
+    $db->update('user', $update_remember);
+}
+?>
