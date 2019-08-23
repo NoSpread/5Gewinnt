5da6589f9fa72e45a9a3e2ae1490272aebcbd79a
Simon <inf18165@lehre.dhbw-stuttgart.de>
2019-08-23 21:06:54 +0200

    fixed problems with logout

--- web/pages/logout.php
+++ web/pages/logout.php
@@ -2,7 +2,7 @@
 require_once '../res/php/config.php';
 // Die Session wird geladen und anschließend alle Variablen gelöscht.
 session_start();
-session_unset();
+session_destroy();
 
 if(isset($_COOKIE['series_id']) && isset($_COOKIE['remember_token'])){
 	// Löschen der vorhandenen Session-Daten
